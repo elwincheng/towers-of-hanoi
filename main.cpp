62b9a467441c56d9f1c5725b611aceada8ef546e
Elwin Cheng <67119805+ElwinCheng@users.noreply.github.com>
2022-05-04 11:23:23 -0700

    delete commented out code

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
+/*
+This program is a command-line version of the game Towers of Hanoi.
+By: Elwin Cheng
+May 4, 2022
+*/
 #include<string>
 #include<math.h>
 #include<iostream>
 #include<vector>
-#include<map>
 #include<iomanip>
 #include<algorithm>
 
@@ -21,12 +25,10 @@ int main()
 {
 	//vector<string> colors = {"Blue", "Purple", "Green", "Red"};
 	vector<string> colors = {"-   ", "---  ", "----- ", "-------"};
-	//map<string, int> 
 	vector<vector<int>> towers = {{3, 2, 1, 0}, {}, {}};
 	while(true)
 	{
-
-		// Display Current game
+		// Display the current game state
 		for (int i = 3; i >= 0; i--)
 		{
 			for (int j = 0; j < 3; j++)
@@ -45,42 +47,29 @@ int main()
 			cout << setw(10) << "(" + letter +  ")  ";
 		}
 
+		// Game win condition
 		if (towers[2].size() == 4)
 		{
 			cout << "\n/****** GAME SOLVED ******/";
 			break;
 		}
 
+		// Ask user for input
+		// e.g. move a to c<ENTER>
 		cout << "\n\n";
 		string command;
 		cout << "Enter a command: ";
 		cin >> command;
-		/*
-		if (toLowerCase(command) == "display")
-		{
-			char tower; 	
-			cin >> tower;
-			int index = tolower(tower) - 'a';
-			for (int i = towers[index].size() - 1; i >= 0; i--)
-			{
-				cout << colors[towers[index][i]] << '\n';
-			}
-		}
-		*/
+
 		string color;
 		char start, end;
 		string to;
 		cin >> start >> to >> end;
-		//cout << color << start << to << end;
-		/*
-		for (int i = 0; i < 4; i++)
-		{
-			if (toLowerCase(colors[i]) == toLowerCase(color))
-				index = i;
-		}
-		*/
+
+		// Clear terminal with linebreaks
 		for (int i = 0; i < 100; i++)
 			cout << '\n';
+
 		if (towers[tolower(start) - 'a'].size() == 0 || start == end)
 		{
 			cout << "/****** INVALID MOVE *******/\n";
@@ -88,11 +77,14 @@ int main()
 		}
 
 		int diskValue = towers[tolower(start) - 'a'].back();
+
 		if (towers[tolower(end) - 'a'].size() && diskValue > towers[tolower(end) -'a'].back())
 		{
 			cout << "/****** INVALID MOVE *******/\n";
 			continue;
 		}
+		
+		// Move disk
 		towers[tolower(end) -'a'].push_back(diskValue);
 		towers[tolower(start) - 'a'].pop_back();
 		cout << "/****** SUCCESSFULLY MOVED ******/\n";
